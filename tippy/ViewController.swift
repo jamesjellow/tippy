ff8965ab5622197d1c304d4940cac20a69837873
jamesjellow <jellowjames@gmail.com>
2020-01-03 17:50:20 -0800

    Finished Calculator App

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var billField: UITextField!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [ 0.15, 0.18, 0.2 ]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipSegment.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 
 }
-
